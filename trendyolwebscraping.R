33d47004c0c933eaa35bbc12592e02affc4f95c5
ezoyedigol <92619993+ezoyedigol@users.noreply.github.com>
2024-03-04 18:29:16 +0300

    Update trendyolwebscraping.R

--- trendyolwebscraping.R
+++ trendyolwebscraping.R
@@ -6,27 +6,27 @@ library(tidyverse)
 
 link = "https://www.trendyol.com/sr?tag=kategori_encoksatanlar"  
 trendyol = read_html(link)
-#urun isimleri
+#ürün isimleri
 product_name = trendyol %>% html_nodes(".prdct-desc-cntnr-name") %>% html_text() 
 
-#urun fiyatlar??
+#ürün fiyatları
 product_prices = trendyol %>% html_nodes(".prc-box-dscntd") %>% html_text()
-# Virg??lleri noktaya d??n????t??r
+# Virgülleri noktaya dönüştür
 product_prices <- gsub(",", ".", product_prices)
-# TL ibaresini kald??r
+# TL ibaresini kaldır
 product_prices <- gsub(" TL", "", product_prices)
-# numerik olarak ??evir
+# numerik olarak çevir
 product_prices <- as.numeric(product_prices)
 
-#urun markalar??
+#ürün markaları
 product_brands = trendyol %>% html_nodes(".prdct-desc-cntnr-ttl") %>% html_text() 
 
-#urun resimleri
+#ürün resimleri
 product_images <- trendyol %>% 
   html_nodes("img") %>%  
   html_attr("src") 
 
-#yorumlar?? ??ekme
+#yorumları çekme
 url <- "https://www.trendyol.com/smool/ingiliz-karbonati-1000-gr-icilebilir-katkisiz-dogal-food-grade-p-750018562/yorumlar?boutiqueId=61&merchantId=710576"
 web_page <- read_html(url)
 
